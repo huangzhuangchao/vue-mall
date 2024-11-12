c24ac02cb39511c5da3d686b5d47b50a79ed514f
huangzhuangchao <2393026981@qq.com>
2024-11-12 21:19:30 +0800

    引入图标库

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,5 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue';
+import LayoutHeader from './components/LayoutHeader.vue';
+import LayoutFooter from './components/LayoutFooter.vue';
+</script>
 <template>
-    <div>我是首页</div>
+    <LayoutNav></LayoutNav>
+    <LayoutHeader></LayoutHeader>
+    <LayoutFooter></LayoutFooter>
     <!-- 二级路由出口 -->
     <RouterView></RouterView>
 </template>
