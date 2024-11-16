af7e8d112cb3371b7d1781383b443464d36c3c27
huangzhuangchao <2393026981@qq.com>
2024-11-16 15:14:00 +0800

    二级分类+路由配置

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,10 +1,38 @@
 import request from '@/utils/http'
 
+//获取一级分类数据
 export function getCategoryAPI(id){
   return request({
     url:'/category',
     params:{
       id
     }
+  })
+}
+
+//获取二级分类列表数据
+export function getCategoryFilterAPI(id) {
+  return request({
+    url: '/category/sub/filter',
+    params: {
+      id
+    }
+  })
+}
+
+
+/** 获取导航数据
+ * * @data {
+     categoryId: 1005000 ,
+     page: 1,
+     pageSize: 20,
+     sortField: 'publishTime' | 'orderNum' | 'evaluateNum'
+   }
+ */
+export function getSubCategoryAPI(data) {
+  return request({
+    url: '/category/goods/temporary',
+    method: 'POST',
+    data
   })
 }
